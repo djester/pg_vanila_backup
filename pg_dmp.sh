76b30eefb7121b55ac9414483b4f6482db13fd68
Dmitry Kremer <djester@users.noreply.github.com>
2026-03-19 12:06:07 +0300

    Create pg_dmp.sh

---
+++ pg_dmp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Command-line parameters
+PG_DMP_DB=$1
+PG_DMP_USER=$2
+PG_DMP_HOST=$3
+PG_DMP_PORT=$4
+PG_DMP_ENCODE=$5
+PG_DMP_ENCODE_DEFAULT=UTF8
+[ "${PG_DMP_ENCODE}" ] || { PG_DMP_ENCODE=${PG_DMP_ENCODE_DEFAULT} ; }
+
+# Build dump command
+PG_DMP_PATH=/mnt/backups/${PG_DMP_HOST}/dumps
+PG_DMP_LOG_PATH=/mnt/backups/${PG_DMP_HOST}/logs
+mkdir -p ${PG_DMP_PATH}
+mkdir -p ${PG_DMP_LOG_PATH}
+PG_DMP_FILE=${PG_DMP_PATH}/${PG_DMP_DB}_$(date +%Y%m%d).dump
+PG_DMP_LOG_FILE=${PG_DMP_LOG_PATH}/dump_${PG_DMP_DB}_$(date +%Y%m%d).log
+PG_DMP_BIN=/usr/bin/pg_dump
+PG_DMP_PARAMETERS=" -v -b -Fc -E ${PG_DMP_ENCODE} -h ${PG_DMP_HOST} -U ${PG_DMP_USER} -p ${PG_DMP_PORT} -f ${PG_DMP_FILE} ${PG_DMP_DB}"
+
+# Run dump
+#echo "debug"
+#echo ${PG_DMP_BIN} ${PG_DMP_PARAMETERS} " 2> " ${PG_DMP_LOG_FILE}
+# run
+${PG_DMP_BIN} ${PG_DMP_PARAMETERS} 2> ${PG_DMP_LOG_FILE}
+exit 0
