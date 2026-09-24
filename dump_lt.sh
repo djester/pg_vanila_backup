a0242264a226219b8543a8f7141ca0262d02d0a9
Dmitry Kremer <djester@users.noreply.github.com>
2026-09-24 16:56:18 +0300

    put dumps into subdirectory

--- dump_lt.sh
+++ dump_lt.sh
@@ -2,8 +2,11 @@
 
 # This script used for prepare dump loaded to other enviroment without data obfuscation
 # For example you should move data from stage to test enviroment
-# This script requerie ini file with connection data for each dumped database
-# Dumps put into same directory as script contained
+# This script requerie 
+# - ini file with connection data for each dumped database
+# - .pgpass with source database credentials
+# - postgres:postgres file owner
+# Dumps put into  subdirectory directory
 
 [ "$1" ] || { echo "No arguments. Argument should be short name of projest (schema name)" >&2; exit 1; }
 
@@ -18,9 +21,11 @@ DMP_PORT=$(grep $1 ${INI_FILE} | awk '{print $3}')
 DMP_HOST=$(grep $1 ${INI_FILE} | awk '{print $4}')
 
 PRJ_NAME=$(echo $1 | sed 's/\_.*//')
-DMP_PATH=${SCRIPT_DIR}
+DMP_PATH=${SCRIPT_DIR}/${DB_NAME}
 DMP_FILE=${DMP_PATH}/${PRJ_NAME}_$(date +%Y%m%d).dump
 GLOBAL_FILE=${DMP_PATH}/global_${PRJ_NAME}.sql
+
+mkdir -p ${DMP_PATH}
 
 pg_dumpall --roles-only -h $DMP_HOST -p ${DMP_PORT} -U postgres > ${GLOBAL_FILE}
 
